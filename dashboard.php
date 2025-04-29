a029b3069ea1f6bd8616dd206ed1d5e1228ffabf
Sagar Raut <sagarraut2059@gmail.com>
2025-04-29 11:38:34 +0545

    admin access

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,13 @@
 <?php 
 session_start(); 
+
+
+// Check if admin is logged in
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+  header("Location: login.php");
+  exit();
+}
+
 include('sidebar.php'); 
 ?>
 
